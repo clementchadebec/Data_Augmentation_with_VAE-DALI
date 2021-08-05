ce64a8303c56703ce718c50533fa458d642c7fd9
clementchadebec <clement.chadebec@orange.fr>
2021-08-05 12:27:36 +0200

    set generation type

--- generation_launcher.sh
+++ generation_launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python generation_parser.py --path_to_model 'trained_models/RHVAE_train_MNIST_ldim_10_nlf_3_epslf_0.01_T_1.5_lbd_0.001/RHVAE_train_MNIST.model' \
---num_samples 100 --verbose --mcmc_steps 100 --generation_method 'manifold_sampling' --seed 8
+--num_samples 100 --verbose --mcmc_steps 100 --generation_method 'metric_sampling' --seed 8
